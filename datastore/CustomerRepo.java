b6a913c328e5d4bbb785c418ef28d3e46da4ba9c
Frank-dev20 <franknjoe2@gmail.com>
2022-08-24 11:13:54 +0100

    Added data store for bank functionality

---
+++ datastore/CustomerRepo.java
@@ -0,0 +1,23 @@
+package com.maven.bank.datastore;
+
+import com.maven.bank.Customer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CustomerRepo {
+
+    private static Map<Long, Customer> customers = new HashMap<>();
+
+    public static Map<Long, Customer> getCustomers() {
+        return customers;
+    }
+
+    private void setCustomers(Map<Long, Customer> customers) {
+        CustomerRepo.customers = customers;
+    }
+
+
+
+
+}
